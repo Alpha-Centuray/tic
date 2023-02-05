d633a6b25bfc8886a144ed0341250269a4bccb4f
Abhishek Choudhary <theabhishek.srm@gmail.com>
2023-02-05 11:32:22 +0530

    ticArray

--- src/App.js
+++ src/App.js
@@ -1,25 +1,98 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import Icon from "./Components/Icon";
+// toastify import
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import "./style.css"
+
+const App = () => {
+    const ticTacToeArray = new Array(9).fill("")
+    const [isCross, setIsCross] = useState(true);
+    const [winMessage, setWinMessage] = useState("");
+
+    // playAgain  
+    function playAgain(){
+        console.log("Play Again is executed")
+        ticTacToeArray.fill("");
+        setIsCross(true);
+        setWinMessage("");
+    }
+
+    // findWinner
+    // 0,1,2  3,4,5   6,7,8
+    function findWinner() {
+        // row
+        for(let i = 0;i<=6; i=i+3){
+            if(ticTacToeArray[i] == ticTacToeArray[i+1] && ticTacToeArray[i] == ticTacToeArray[i+2] && ticTacToeArray[i] != ""){
+                setWinMessage(ticTacToeArray[i] + " has won");
+            }
+        }
+
+        // column => 0,3,6  1,4,7  2,5,8
+
+        for(let i = 0;i<=2; i=i+1){
+            if(ticTacToeArray[i] == ticTacToeArray[i+3] && ticTacToeArray[i] == ticTacToeArray[i+6] && ticTacToeArray[i] != ""){
+                setWinMessage(ticTacToeArray[i] + " has won");
+            }
+        }
+
+        // diagonal => 0,4,8  2,4,6
+        if(ticTacToeArray[0] == ticTacToeArray[4] && ticTacToeArray[0] == ticTacToeArray[8] && ticTacToeArray[0] != ""){
+            setWinMessage(ticTacToeArray[0] + " has won");
+        }
+
+        if(ticTacToeArray[2] == ticTacToeArray[4] && ticTacToeArray[2] == ticTacToeArray[6] && ticTacToeArray[2] != ""){
+            setWinMessage(ticTacToeArray[2] + " has won");
+        }
+        
+        // draw condition pending
+        
+    }
+
+    // changeItem
+
+    function changeItem(index){
+        // game over case 
+        if(ticTacToeArray[index] != ""){
+            return toast.error("Allready filled");
+        }
+        else if(ticTacToeArray[index] == ""){
+             ticTacToeArray[index] =  isCross==true? "cross" : "circle"
+             setIsCross(!isCross);
+             findWinner();         
+        }
+        
+    }
+    return (
+        <div className="tic">
+            <h1 className="top-heading">Tic Tac Toe</h1>
+            {
+                winMessage==""? (
+                   isCross==true? (
+                    <h2 className="heading-chance">Cross Chance</h2>
+                   ) : (
+                    <h2 className="heading-chance">Cross Chance</h2>
+                   )
+                ) : 
+                (
+                   <div className="gameOver"> 
+                      <h1>{winMessage}</h1>
+                      <button onClick={playAgain}>Play Again</button>
+                   </div> 
+                )
+            }
+            <div className="grid">
+               {
+                ticTacToeArray.map((value, index)=>(
+                    <div onClick={()=>{changeItem(index)}}>
+                        <Icon choice={ticTacToeArray[index]}/>
+                    </div>
+                ))
+               }
+            </div>
+        </div>
+    );
 }
 
 export default App;
